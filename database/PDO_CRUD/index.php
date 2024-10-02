5db9c60a4ea8e91af73df3b0294ed76e54308a7d
Havi Shah <166846385+havishahh@users.noreply.github.com>
2024-10-02 18:46:11 +0530

    index.php

--- database/PDO_CRUD/index.php
+++ database/PDO_CRUD/index.php
@@ -64,14 +64,10 @@
                                 <input id="author" name="author" type="text" placeholder="" class="form-control input-md" required="">
                             </div>
                         </div>
-
-                        <!-- Button -->
-                        <div class="form-group">
-                            <label class="col-md-12 control-label" for="submit"></label>
-                            <div class="col-md-12">
-                                <button id="submit" name="submit" value="insert" class="btn btn-primary">Insert</button>
-                            </div>
-                        </div>
+                                <button id="insert" name="submit" value="insert" class="btn btn-primary">Insert</button>
+                                <button id="update" name="submit" value="update" class="btn btn-primary" style="display: none;">Update</button>
+                                <button id="delete" name="submit" value="delete" class="btn btn-primary" style="display: none;">Delete</button>
+                                <input type="hidden" name="bid" id="bid"  value="">
                     </fieldset>
                 </form>
             </div>
@@ -91,15 +87,73 @@
                     </thead>
                     <tbody>
                         <?php
-                            if(isset($_GET['insert']) && $_GET['insert']==1){
+                            if(isset($_GET['insert']) && $_GET['insert']==1)
+                            {
                                 echo "inserted successfully";
-                            }else{
-                                echo "something went wrong";
+                            }
+                            elseif(isset($_GET['insert']) && $_GET['insert']==0)
+                            { 
+                                echo "something went wrong while inserting";
+                            }
+                            elseif(isset($_GET['update']) && $_GET['update']==1)
+                            {
+                                    echo "updated successfully";
+                            }elseif(isset($_GET['update']) && $_GET['update']==0)
+                            {
+                                echo "something went wrong while updating";
+                            }
+                            elseif(isset($_GET['delete']) && $_GET['delete']==1)
+                            {
+                                echo "deleted successfully";
+                            }elseif(isset($_GET['delete']) && $_GET['delete']==0)
+                            {
+                            echo "something went wrong while deleting";
+                            }
+
+                            require_once('book.php');
+                            $objBook = new Book;
+                            $books = $objBook->getAllBooks();
+                            foreach($books as $book){
+                                $data = json_encode($book,true);
+                                echo "<tr>";
+                                    echo "<td>".$book['id']."</td>";
+                                    echo "<td>".$book['name']."</td>";
+                                    echo "<td>".$book['type']."</td>";
+                                    echo "<td>".$book['pages']."</td>";
+                                    echo "<td>".$book['price']."</td>";
+                                    echo "<td>".$book['author']."</td>";
+                                    echo "<td>".$book['created_date']."</td>";
+                                    echo "<td><a href='javascript:updateBook($data)'>edit</a></td>";
+                                    echo "<td><a href='javascript:deleteBook($data)'>delete</a></td>";
+                                    echo "</tr>";
                             }
                         ?>
                     </tbody>
                 </table>
             </div>
         </div>
+        <script type="text/javascript">
+            function updateBook(book) {
+                document.getElementById("bid").value = book.id;
+                document.getElementById("bname").value = book.name;
+                document.getElementById("btype").value = book.type;
+                document.getElementById("pages").value = book.pages;
+                document.getElementById("price").value = book.price;
+                document.getElementById("author").value = book.author;
+                document.getElementById("insert").style.display = "none";
+                document.getElementById("update").style.display = "block";   
+            }
+
+            function deleteBook(book) {
+                document.getElementById("bid").value = book.id;
+                document.getElementById("bname").value = book.name;
+                document.getElementById("btype").value = book.type;
+                document.getElementById("pages").value = book.pages;
+                document.getElementById("price").value = book.price;
+                document.getElementById("author").value = book.author;
+                document.getElementById("insert").style.display = "none";
+                document.getElementById("delete").style.display = "block";   
+            }
+        </script>
 </body>
 </html>
