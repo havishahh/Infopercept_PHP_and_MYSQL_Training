a1e2c6ddbc8e51d834cd0bcda8cec505d10b7f0b
Havi Shah <166846385+havishahh@users.noreply.github.com>
2024-10-02 18:47:53 +0530

    action.php

--- database/PDO_CRUD/action.php
+++ database/PDO_CRUD/action.php
@@ -18,6 +18,32 @@
                         header('location: index.php?insert=0');
                     }
                     break;
+
+                    case 'update':
+                        $objBook = new Book;
+                        $objBook->setId($_POST['bid']);
+                        $objBook->setName($_POST['bname']);
+                        $objBook->setType($_POST['btype']);
+                        $objBook->setPages($_POST['pages']);
+                        $objBook->setPrice($_POST['price']);
+                        $objBook->setAuthor($_POST['author']);
+                        if($objBook->update()){
+                            header('location: index.php?update=1');
+                        }else{
+                            header('location: index.php?update=0');
+                        }
+                        break;
+
+                case 'delete':
+                    $objBook = new Book;
+                        $objBook->setId($_POST['bid']);
+                        if($objBook->delete()){
+                            header('location: index.php?delete=1');
+                        }else{
+                            header('location: index.php?delete=0');
+                        }
+                        break;
+
                 case 'default':
                     header('location: index.php');
                     break;
